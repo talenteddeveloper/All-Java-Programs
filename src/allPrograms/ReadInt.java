3de2cc3c0dc3a2aa1b202ef69458ad7e6609a43a
Mr. Rohit <singh.rohitrajput94@gmail.com>
2020-03-08 11:52:30 +0530

    Read Int

---
+++ src/allPrograms/ReadInt.java
@@ -0,0 +1,17 @@
+package allPrograms;
+
+import java.util.Scanner;
+
+public class ReadInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter a number :");
+		
+		int input = reader.nextInt();
+		System.out.println("Your number is :"+input);
+	}
+
+}
