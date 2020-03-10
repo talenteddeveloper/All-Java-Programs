3a3f337793f6d13a0cfe755222b68518585cfa9a
Mr. Rohit <singh.rohitrajput94@gmail.com>
2020-03-10 17:06:31 +0530

    Read String

---
+++ src/allPrograms/ReadString.java
@@ -0,0 +1,16 @@
+package allPrograms;
+
+import java.util.Scanner;
+
+public class ReadString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter any word :");
+		String word = reader.next();
+		System.out.println("my word is :"+word);
+		
+	}
+
+}
