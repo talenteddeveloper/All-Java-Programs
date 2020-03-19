9d7b467f4b06fd5acf36f60b881f5fe669b65e69
Mr. Rohit <singh.rohitrajput94@gmail.com>
2020-03-19 21:46:28 +0530

    Java Program to find ASCII value

---
+++ src/allPrograms/AsciiValue.java
@@ -0,0 +1,14 @@
+package allPrograms;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char myword='A';
+		int ascii = myword;
+		int ascii1 = (int)myword;
+		System.out.println(ascii);
+		System.out.println(ascii1);
+	}
+
+}
