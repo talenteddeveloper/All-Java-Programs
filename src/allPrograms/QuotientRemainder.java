f1831991f1b6f8cb84bfa15beb52cf7eff08a4e4
Mr. Rohit <singh.rohitrajput94@gmail.com>
2020-04-07 16:47:30 +0530

    java find quotient and remainder

---
+++ src/allPrograms/QuotientRemainder.java
@@ -0,0 +1,15 @@
+package allPrograms;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		int dividend=25, divisor=4;
+		
+		int quotient = dividend / divisor;
+		int remainder = dividend % divisor;
+		
+		System.out.println("Quo :"+quotient);
+		System.out.println("Rem :"+remainder);
+	}
+
+}
