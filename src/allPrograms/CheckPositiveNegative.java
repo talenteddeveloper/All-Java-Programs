f0d6705eeb95a1339abe8be82cd788afeafc1e26
talenteddeveloper <programmersworldz@gmail.com>
2021-01-15 02:29:54 +0530

    Java Program to Check Whether a Number is Positive or Negative

---
+++ src/allPrograms/CheckPositiveNegative.java
@@ -0,0 +1,20 @@
+package allPrograms;
+
+public class CheckPositiveNegative {
+
+	public static void main(String[] args) {
+		
+		double number =-6560.0;
+		
+		if(number >0.0)
+		{
+			System.out.println(number +" is a positive number");
+		}
+		else if(number < 0.0) {
+			System.out.println(number +" is a negative number");
+		}else {
+			System.out.println(number +" is 0");
+		}
+	}
+
+}
