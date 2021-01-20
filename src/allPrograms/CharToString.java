819a1809fd69614cec8230aec418d7fa31f39072
talenteddeveloper <programmersworldz@gmail.com>
2021-01-21 00:12:25 +0530

    Java Program to Convert char to String

---
+++ src/allPrograms/CharToString.java
@@ -0,0 +1,14 @@
+package allPrograms;
+
+public class CharToString {
+
+	public static void main(String[] args) {
+		char c='a';
+		String s1= Character.toString(c);
+		String s2= String.valueOf(c);
+		System.out.println("The string: "+s1);
+		System.out.println("The string1: "+s2);
+		
+	}
+
+}
