fc55f2ac8f0d7d72825896c1a107a8886669d2c9
talenteddeveloper <programmersworldz@gmail.com>
2021-01-26 21:03:12 +0530

    Java String to char array

---
+++ src/allPrograms/StringToCharArray.java
@@ -0,0 +1,14 @@
+package allPrograms;
+
+import java.util.Arrays;
+
+public class StringToCharArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String word ="I love food";
+		char[] chars = word.toCharArray();
+		System.out.println(Arrays.toString(chars));
+	}
+
+}
