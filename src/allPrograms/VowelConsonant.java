2bb51f884f8e4d30ce7b5a7a369285c7ac6053cb
Devil <programmersworldz@gmail.com>
2022-05-09 15:21:47 +0530

    java program to Check whether an alphabet is vowel or consonant using if..else statement

---
+++ src/allPrograms/VowelConsonant.java
@@ -0,0 +1,15 @@
+package allPrograms;
+
+public class VowelConsonant {
+
+	public static void main(String[] args) {
+		
+		char c='i';
+		if(c =='a' ||c=='e' || c=='i' || c=='o' || c=='u') {
+			System.out.println(c +" is vowel");
+		}else {
+			System.out.println(c + " is consonant");
+		}
+	}
+
+}
