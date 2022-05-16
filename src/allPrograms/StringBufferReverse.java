43bb8396bc9f409e7d34e8390a9525cab96f22b1
Devil <programmersworldz@gmail.com>
2022-05-16 16:58:11 +0530

    Java Program to reverse a String Using StringBuffer

---
+++ src/allPrograms/StringBufferReverse.java
@@ -0,0 +1,12 @@
+package allPrograms;
+
+public class StringBufferReverse {
+
+	public static void main(String[] args) {
+		String input = "abcd";
+		StringBuffer sb= new StringBuffer(input);
+		System.out.println(sb.reverse());
+		
+	}
+
+}
