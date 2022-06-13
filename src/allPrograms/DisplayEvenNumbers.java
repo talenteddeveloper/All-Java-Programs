2237fc0efeeade10e70f6a38363d32e08e4cc157
Devil <programmersworldz@gmail.com>
2022-06-13 15:01:26 +0530

    Java program to display even numbers from 1 to 100

---
+++ src/allPrograms/DisplayEvenNumbers.java
@@ -0,0 +1,15 @@
+package allPrograms;
+
+public class DisplayEvenNumbers {
+
+	public static void main(String[] args) {
+		int number=100;
+		for(int i=1; i<=number; i++) {
+			//System.out.println(i);
+			if(i%2 ==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
